0ee7c5e8ee64adbb3cbfe0937eae174da2a62806
punitha <punitha1111@gmail.com>
2023-03-10 12:25:22 +0530

    this file is commited

---
+++ src/day01/SampleFile1.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class SampleFile1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is Sample File 1");
+	}
+
+}
